7e0ae83502de52ecba5659885e13d6ce05a32800
霊音・永旻 <reineimi.dev@gmail.com>
2024-10-31 20:41:23 +0400

    Add files via upload

---
+++ lib/va2.lua
@@ -0,0 +1,51 @@
+--	This code and all included modules are licensed
+--	under VPCDP by Eimi Rein (霊音 永旻) - @reineimi
+-->		https://github.com/reineimi/VPCDP
+_ENV.va2 = {
+	-- Includes
+	modules = {
+		'response',
+		'lib.va2bash',
+		'lib.va2crawl',
+		'lib.va2tables'
+	},
+	-- Library environment
+	env = {},
+	-- Module environment
+	mod = {},
+	-- Enums
+	e = {},
+	-- Magic numbers
+	n = {},
+	-- Temporary data
+	temp = {}
+}
+
+-- Library initialization
+local va2_init = function()
+	math.randomseed(os.time())
+
+	-- Get current system user
+	local p = io.popen('whoami')
+	_G.USER = p:read('*a'):gsub('\n', '')
+	p:close()
+
+	-- Construct PATH
+	local PATH = {
+		'/srv/http/va2.reineimi',
+		'/home/'..USER..'/Documents/va2.reineimi',
+	}
+	for _, path in ipairs(PATH) do
+		package.path = path..'/?.lua;'..package.path
+	end
+
+	-- Include modules
+	for _, M in ipairs(va2.modules) do
+		local STATUS, RES = pcall(require, M)
+		if STATUS then
+			_G[M:match('[.](.+)')] = RES
+		end
+	end
+end
+
+va2_init()
--- lib/va2crawl.lua
+++ lib/va2crawl.lua
@@ -1,108 +1,24 @@
 -- URL crawler by @reineimi [https://github.com/reineimi]
+--require 'va2bash'
 local crawl = { temp={}, sitemap={}, pages={} }
 
--- Get HTML document from URL
-function curl(url)
-	local p = io.popen('curl -s \''..url..'\'')
-	local out = p:read('*a')
-	p:close()
-	return out
-end
-
--- Styled bash stdout
--- ex: crawl.out({'Hello', 'grey'}, {'world!', 'green', 'bold'})
-crawl.printf = {
-	-- Style
-	normal = 0,
-	bold = 1, b = 1,
-	dark = 2,
-	italic = 3, i = 3,
-	underline = 4, u = 4,
-	blink = 5,
-	blinkDark = 6,
-	invert = 7,
-	empty = 8,
-	strike = 9, s = 9,
-
-	-- Color
-	black = 30,
-	darkred = 31,
-	darkgreen = 32,
-	brown = 33,
-	darkblue = 34,
-	darkpurple = 35,
-	darkcyan = 36,
-	lightgrey = 37,
-
-	grey = 90,
-	red = 91,
-	green = 92,
-	yellow = 93,
-	blue = 94,
-	purple = 95,
-	cyan = 96,
-	white = 97,
-
-	-- Background color
-	blackBg = 40,
-	darkredBg = 41,
-	darkgreenBg = 42,
-	brownBg = 43,
-	darkblueBg = 44,
-	darkpurpleBg = 45,
-	darkcyanBg = 46,
-	lightgreyBg = 47,
-
-	greyBg = 100,
-	redBg = 101,
-	greenBg = 102,
-	yellowBg = 103,
-	blueBg = 104,
-	purpleBg = 105,
-	cyanBg = 106,
-	whiteBg = 107
-}
-function crawl.out(...)
-	local inputs, strings = {...}, {}
-
-	for _, strdata in ipairs(inputs) do
-		local str = tostring(strdata[1]):gsub('"', '\\"')
-		local _formats = {}
-
-		-- Find by key or value
-		for i, v in pairs(crawl.printf) do
-			for n = 2, 4 do
-				local arg = strdata[n]
-				if arg and ((arg == i) or (arg == v)) then
-					table.insert(_formats, v)
-				end
-			end
-		end
-
-		table.insert(strings, string.format(
-			'\\e[0;%sm%s\\e[0m',
-			table.concat(_formats, ';'), str
-		))
-	end
-
-	os.execute('echo -e "> '..table.concat(strings, ' ')..'"')
-end
-local out = crawl.out
-
 -- Crawl the page, check/generate SEO content
-function crawl:loop(url, args)
+function crawl:loop(url, _args)
+	local _args = _args or {}
 	local domain = url:match('http[s]?://(.+)'):gsub('/.*', '')
 	local html = curl(url)
 	local http = 'http://'
-	if args.ssl then http = 'https://' end
-	local GMT = args.gmt or '+00:00'
-	local out, print = out, print
-	if not args.out then
-		out = function() end
+	if _args.ssl then http = 'https://' end
+	local excludes = _args.exclude or {}
+	local GMT = _args.gmt or '+00:00'
+	local print, echo = print, echo, echoF
+	if not _args.out then
 		print = function() end
+		echo = function() end
+		echoF = function() end
 	end
 	if not html:match('<!DOCTYPE html>') then
-		out({'Page is not of the HTML type, does not exist, or has been redirected', 'grey'})
+		echo('#grey;Page is not of the HTML type, does not exist, or has been redirected;')
 		print ''
 		return 0
 	end
@@ -117,7 +33,7 @@ function crawl:loop(url, args)
 	if #self.sitemap == 0 then
 		self.total = 0
 		self.urls = {}
-		out({'0:', 'yellow'}, {url}, {' 1.0', 'cyan'})
+		echo('#yellow;0:; #blue;'..url..';  #cyan;1.0;')
 
 		-- Add sitemap sources
 		table.insert(self.sitemap,
@@ -131,39 +47,40 @@ function crawl:loop(url, args)
 	'<url>%s%s%s</url>',
 	'<loc>'..url..'</loc>',
 	'<lastmod>'..os.date('%Y-%m-%dT%H:%M:%S')..GMT..'</lastmod>',
-	'<priority>'..(args.prio or '1.0')..'</priority>'))
+	'<priority>'..(_args.prio or '1.0')..'</priority>'))
 
 	-- Check SEO metatags
 	local title = html:match('<title.->(.-)</title>')
 	page.title = title
 	if title then
 		if title ~= '' then
-			out({'Title:', 'bold'}, {title:gsub('\n', ' '), 'green'})
+			echo('#b;Title:; #green;'..title:gsub('\n', ' ')..';')
 		else
-			out({'Title:', 'bold'}, {'(Empty)', 'yellow'})
+			echo('#b;Title:; #yellow;(Empty);')
 		end
 	else
-		out({'Title tag is missing or did not load initially', 'red'})
+		echo('#red;Title tag is missing or did not load initially;')
 	end
 
 	local descr = html:match('<meta.-name[=]["\']description["\'].-content=["\'](.-)["\'].->')
 	page.descr = descr
 	if descr then
 		if descr ~= '' then
-			out({'Description:', 'bold'}, {descr, 'green'})
+			echoF({'Description: ', 'b'}, {descr, 'green'})
 		else
-			out({'Description:', 'bold'}, {'(Empty)', 'yellow'})
+			echo('#b;Description:; #yellow;(Empty);')
 		end
 	else
-		out({'Description metatag is missing or did not load initially', 'red'})
+		echo('#red;Description metatag is missing or did not load initially;')
 	end
 
 	-- Check for missing alts in <img>
 	for img in html:gmatch('<img .->') do
-		if not img:match('[ ]?alt=["\']') then
+		local img = img:gsub('\n', ' ')
+		if not img:match('[ ]?alt[= >]') then
 			local err = 'Image missing alt attribute:'
 			table.insert(page.err, err..' '..img)
-			out({err, 'red'}, {img, 'lightgrey'})
+			echo('#red;'..err..'; #lightgrey;'..img..';')
 		end
 	end
 
@@ -171,9 +88,17 @@ function crawl:loop(url, args)
 	print ''
 	for link in html:gmatch('href=[\'"](.-)[\'"].->') do
 		local fdomain = domain:gsub('[-]', '[-]'):gsub('[+]', '[+]'):gsub('[.]', '[.]')
-		local inner = link:gsub(http..domain, ''):gsub('^/', ''):gsub('/$', '')
+		local inner = link:gsub(http..fdomain, ''):gsub('^/', ''):gsub('/$', '')
+		local included = true
 
-		if (inner:len() > 1)
+		for _, excl in ipairs(excludes) do
+			local excl = excl:gsub('^/', ''):gsub('[-]', '[-]'):gsub('[+]', '[+]'):gsub('[.]', '[.]')
+			if inner:match(excl) then
+				included = false
+			end
+		end
+
+		if (inner:len() > 1) and included
 		and not (inner:match('[?#!@$%^&*:;]') or inner:match('www'))
 		and not (inner:match('[/]?[.][%a%d]+$'))
 		and not (link:match('wp[-]content/') or link:match('wp[-]json')) then
@@ -182,14 +107,14 @@ function crawl:loop(url, args)
 			local __, prio = rel:gsub('/', '')
 			local prio = 1.1 - (prio * 0.1)
 			if prio < 0.1 then prio = 0.1 end
-			args.prio = prio
+			_args.prio = prio
 
 			-- Check if URL was already crawled -> crawl new URL
 			if not self.urls[rel] then
 				self.total = self.total + 1
-				out({self.total..':', 'yellow'}, {rel}, {' '..prio, 'cyan'})
+				echo('#yellow;'..self.total..':; '..rel..'  #cyan;'..prio..';')
 				self.urls[rel] = 1
-				crawl:loop(ref, args)
+				crawl:loop(ref, _args)
 			end
 		end
 	end
@@ -197,37 +122,36 @@ function crawl:loop(url, args)
 end
 
 --[[ (USE THIS) Wrap and finish crawl:loop()
-<str> State (generated content):
+<int> _state (generated content):
 	nil: Do nothing
 	1: Print
 	2: Write to file
-[<table>] Args:
-	out: Print results?
-	gmt: sitemap.xml timezone
+[<tbl>] _args:
+	<int> out: Print results?
+	<str> gmt: sitemap.xml timezone
+	<tbl> exclude: Inner links to exclude
 --]]
-function crawl:run(url, State, Args)
-	local loop_args = {}
-	local out, print = out, print
-	if not Args.out then
-		out = function() end
+function crawl:run(url, _state, _args)
+	local _args = _args or {}
+	local print, echo = print, echo, echoF
+	if not _args.out then
 		print = function() end
+		echo = function() end
+		echoF = function() end
 	end
 	if url:len() < 2 then
-		out({'Specified URL is too short', 'red'})
+		echo('#red;Specified URL is too short;')
 		return 0
 	end
 	if not url:match('http[s]?://') then
 		url = 'https://'..url
-		out({'Reading specified URL in HTTPS mode', 'yellow'})
+		echo('#cyan;Reading specified URL in HTTPS mode;')
 	end
 	local SSL = nil
-	local GMT = Args.gmt
-	if GMT == '' then GMT = nil end
+	if _args.gmt == '' then _args.gmt = nil end
 	if url:match('https://') then SSL = 1 end
 	if not url:match('/$') then url = url..'/' end
-	loop_args.out = Args.out
-	loop_args.gmt = GMT
-	loop_args.ssl = SSL
+	_args.ssl = SSL
 	self.sitemap = {}
 	self.urls = {}
 	self.total = 1
@@ -237,68 +161,93 @@ function crawl:run(url, State, Args)
 	-- Check redirects
 	local redir = curl(url..'/') or ''
 	if redir:match('<html') then
-		out({'Redirects -'}, {'Unset or incomplete', 'red'})
+		echo('Redirects - #red;Unset or incomplete;')
 	else
-		out({'Redirects -'}, {'Working', 'green'})
+		echo('Redirects - #green;Working;')
 	end
 
 	-- Look for current sitemap
 	local current_sitemap = curl(url..'/sitemap.xml')
 	if current_sitemap:match('<urlset') then
 		mainpage['sitemap.xml'] = true
-		out({'sitemap.xml -'}, {'Found', 'cyan'})
+		echo('sitemap.xml - #green;Found;')
 	else
 		mainpage['sitemap.xml'] = false
-		out({'sitemap.xml -'}, {'Not found', 'red'})
+		echo('sitemap.xml - #red;Not found;')
 	end
 
 	-- Check robots.txt
 	local robots = curl(url..'/robots.txt')
-	if robots:match('Host:') then
+	if robots:match('[Uu]ser[-][Aa]gent:') then
 		mainpage['robots.txt'] = true
-		out({'robots.txt -'}, {'Found', 'green'})
+		echo('robots.txt - #green;Found;')
+		if not robots:match('Host:') then
+			echo('robots.txt: #red;No host(s) set;')
+		end
 		local bots = 0
 		for bot in robots:gmatch('[Uu]ser[-][Aa]gent:[ ]?([%a%d%p]+)') do
-			out({'robots.txt:'}, {'Agent found: '..bot, 'cyan'})
+			echo('robots.txt: #cyan;Agent found: '..bot..';')
 			bots = bots + 1
 		end
 		if bots < 1 then
-			out({'robots.txt:'}, {'No agents set', 'yellow'})
+			echo('robots.txt: #red;No agents set;')
 		elseif bots < 2 then
-			out({'robots.txt:'}, {'Only one agent set', 'yellow'})
+			echo('robots.txt: #yellow;Only one agent set;')
+		end
+		local sitemap_alt = robots:match('Sitemap: (.-)\n')
+		if sitemap_alt then
+			echo('robots.txt: #cyan;Alternative sitemap.xml found at:; #blue;'..sitemap_alt..';')
+			if curl(sitemap_alt):match('schemas/sitemap') then
+				echo('sitemap.xml (Alt) - #green;Found;')
+			end
 		end
 	else
 		mainpage['robots.txt'] = false
-		out({'robots.txt -'}, {'Not found', 'red'})
+		echo('robots.txt - #red;Not found;')
 	end
 
 	-- Crawl cleanup process
 	print ''
-	crawl:loop(url, loop_args)
+	crawl:loop(url, _args)
 	table.insert(self.sitemap, '</urlset>')
 	local sitemap_content = table.concat(self.sitemap, '\n')
 	self.sitemap = {}
 
 	-- Generate sitemap.xml
-	if State == 2 then
+	if _state == 2 then
 		local sitemap_xml = io.open('sitemap.xml', 'w')
 		sitemap_xml:write(sitemap_content)
 		sitemap_xml:close()
-	elseif State == 1 then
+	elseif _state == 1 then
 		print('\n'..sitemap_content)
 	end
 
 	return crawl.pages
 end
 
---[[ Uncomment this to use in the terminal
-local meta = { out=1 }
-out({'Please specify the URL of the website to crawl:', 'cyan', 'i'})
-local addr = io.read()
-out({'...And your (GMT) timezone (default: +00:00):', 'cyan', 'i'})
-meta.gmt = io.read()
-out({'Crawling in progress...', 'grey', 'b'})
-local sitemap = crawl:run(addr, 2, meta)
---]]
+function crawl:run_in_shell()
+	local meta = { out=1 }
+	echo '#cyan;#i;Please specify the URL of the website to crawl:;'
+	local addr = io.read()
+	echo '#cyan;#i;...And your (GMT) timezone (default: +00:00):;'
+	meta.gmt = io.read()
+	echo '#cyan;#i;You can also exclude inner pages (separated with whitespaces):;'
+	local exclude = io.read()..' '
+	meta.exclude = {}
+	for rel in exclude:gmatch('(/.-)[ ]') do
+		table.insert(meta.exclude, rel)
+	end
+	echo('-- String color rules --',
+		'#green;#i;#b;Valid; - #i;No actions needed;',
+		'#cyan;#i;#b;Info; - #i;Advised to check;',
+		'#yellow;#i;#b;Warning; - #i;Advised to take action;',
+		'#red;#i;#b;Error; - #i;Needs to be fixed;',
+		'#grey;#i;#b;Ignored; - #i;Has been ignored for a specific reason;',
+		'#lightgrey;#i;#b;Reference; - #i;Reference to object in context;',
+		'#blue;#i;#b;Link; - #i;Link to resource;',
+		'\n#grey;#i;Crawling in progress...;')
+	crawl:run(addr, 2, meta)
+end
+--crawl:run_in_shell()
 
 return crawl
